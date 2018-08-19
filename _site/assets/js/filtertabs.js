b9be1642c18bed8b054424f7512c7d0426486332
Aashman Goghari <aashman.g@gmail.com>
2018-08-19 17:58:24 -0400

    all stuff

---
+++ _site/assets/js/filtertabs.js
@@ -0,0 +1,20 @@
+
+function filterProject(evt,projectType) {
+  var allProjects = Array.from(document.getElementsByClassName('project'));
+  var selectedProjects = Array.from(document.getElementsByClassName('project ' + projectType));
+  allProjects.forEach(function(e) {
+    e.style = "display: none;opacity: 0;";
+
+  });
+
+  $.each( selectedProjects, function( index, value ){
+    value.style = "display: block;opacity: 0;"
+    $(value).delay(50).animate({opacity:'1',bottom: '20px'},500);
+});
+
+tablinks = Array.from(document.getElementsByClassName("tablink"));
+tablinks.forEach(function(e) {
+    e.className = e.className.replace(" w3-red", "");
+  });
+evt.currentTarget.className += " w3-red";
+}
---
+++ assets/js/pageload.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+
+    $('div.hidden').delay(500).animate({opacity:'1'},800);
+    $( '.project' ).delay(500).animate({bottom: '20px'},800);
+
+});
